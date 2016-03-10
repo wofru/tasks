828e8d84974f4d5175c6a69f58b5dcce6abe6985
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2016-03-10 16:42:26 -0500

    update

---
+++ 05-website/02-gen-landing-page.py
@@ -0,0 +1,42 @@
+import os 
+from jinja2 import Environment, FileSystemLoader
+
+webLoc = '/Users/danielmsheehan/GitHub/wofru.github.com/'
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+
+
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+
+def create_index_html():
+    fname = webLoc+"index.html"
+    urls = ['https://mapzen.com/', 'http://nygeog.com']
+    context = {
+  #       'urls': urls,
+  #       'wofName': wofName,
+		# 'wofFile': wofFile,
+		# 'wofBoun': wofBoun,
+		# 'NELat': NELat,
+		# 'NELng': NELng,
+		# 'SWLat': SWLat,
+		# 'SWLng': SWLng,
+    }
+    #
+    with open(fname, 'w') as htmlFile:
+        html = render_template('index_landing.html', context)
+        htmlFile.write(html)
+    
+
+def main():
+    create_index_html()
+
+#############################################################################
+
+if __name__ == "__main__":
+    main()
